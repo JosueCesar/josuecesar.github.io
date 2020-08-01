f3ae525b0bab0a7970c589f22025f23eecd3d373
Josué Cesar <mendonca.josue@gmail.com>
2020-08-01 15:22:14 -0300

    Updates

---
+++ precache-manifest.ee18479254c562a103a50dcd74fc5a80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb22a6fff94814ead63065ecf86880a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a3b6ffd50a01398299",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55e64eeb459e077953c6",
+    "url": "/static/js/2.04ee6a13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04ee6a13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a3b6ffd50a01398299",
+    "url": "/static/js/main.65015ace.chunk.js"
+  },
+  {
+    "revision": "e512f06415039730d574",
+    "url": "/static/js/runtime-main.434cf78f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
